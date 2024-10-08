053f2ece3c7d80110ef9e691509a829908c560bb
Damien Erambert <damien@erambert.me>
2024-10-07 22:07:54 -0700

    :wrench:

--- src/app.tsx
+++ src/app.tsx
@@ -82,39 +82,6 @@ export function App() {
     }
   };
 
-  const renderEmptyContent = () => {
-    return (
-      <div class="starting">
-        <div class="text">
-          <p>
-            This is a simple tool to easily filter through/visualize your
-            "find-your-friends.json" file you got from your{" "}
-            <a href="https://cohost.org">cohost</a> data export.
-          </p>
-          <p>
-            Your data is <strong>not transmitted anywhere</strong>, and will be
-            saved in your browser's storage so you can come back to this page
-            later on.
-          </p>
-          <p>
-            Just click the button below, select your `find-your-friends.json`
-            file on your computer, and you're done!
-          </p>
-        </div>
-        <input
-          ref={inputRef}
-          type="file"
-          accept="application/json"
-          onInput={onFileChange}
-          style={{ display: "none" }}
-        />
-        <button onClick={onImportClick}>
-          Import your find-your-friends.json file
-        </button>
-      </div>
-    );
-  };
-
   const renderContactLink = (
     contactLink: FindYourFriends[number]["contactCard"][number]
   ) => {
