9f60379ffd088282b7e572665b4b45c61bffd129
Damien Erambert <damien@erambert.me>
2024-10-10 10:42:49 -0700

    Update contactBlock.tsx

--- src/components/contactBlock.tsx
+++ src/components/contactBlock.tsx
@@ -38,7 +38,7 @@ export function ContactBlock(props: ContactBlockProps) {
     <div class="contact-block">
       <div class="contact-header">
         <img
-          src={`http://5.78.79.214:5555/avatar?handle=${friend.handle}`}
+          src={`https://cohost-avatar-proxy.vercel.app/avatar/${friend.handle}`}
           alt=""
           height={30}
           width={30}
