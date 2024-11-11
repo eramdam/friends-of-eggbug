5eb96fc44de719e858ac76b6fc8afa4cfa1af0f2
Damien Erambert <damien@erambert.me>
2024-11-11 02:09:32 -0800

    nitpick

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import "@total-typescript/ts-reset";
-import { render } from "preact";
-import { App } from "./app.tsx";
-import "./index.css";
-
 import "@fontsource/atkinson-hyperlegible/400.css";
 import "@fontsource/atkinson-hyperlegible/700.css";
 import "@fontsource/atkinson-hyperlegible/400-italic.css";
 import "@fontsource/atkinson-hyperlegible/700-italic.css";
+import "./index.css";
+import "@total-typescript/ts-reset";
+import { render } from "preact";
+import { App } from "./app.tsx";
+
 import { Migrator } from "./components/migrator.tsx";
 
 function Main() {
